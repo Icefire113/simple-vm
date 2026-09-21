fc90be71531f8bd6958a0417ca3421476f39ba3f
Icefire113 <58379875+Icefire113@users.noreply.github.com>
2026-09-20 23:43:50 -0700

    a simple test (just the code i already wrote)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use anyhow::Context;
 
 use crate::{
-    assembler::{parser::Parser, tokenizer::tokenizer::Tokenizer},
+    assembler::{
+        parser::Parser,
+        tokenizer::{token::Token, tokenizer::Tokenizer},
+    },
     vm::VM,
 };
 
@@ -13,7 +16,7 @@ fn main() -> anyhow::Result<()> {
     let asm_path = std::env::args().nth(1).context("No source file given")?;
     let asm_text = std::fs::read_to_string(asm_path).context("Failed to read file")?;
 
-    let tokens: Vec<assembler::tokenizer::token::Token> = Tokenizer::new(&asm_text)
+    let tokens: Vec<Token> = Tokenizer::new(&asm_text)
         .tokenize()
         .context("Failed to tokenize")?;
 
@@ -26,4 +29,55 @@ fn main() -> anyhow::Result<()> {
     println!("Result: {:?}", r);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test1() {
+        use crate::assembler::{
+            parser::Parser,
+            tokenizer::{token::Token, tokenizer::Tokenizer},
+        };
+
+        let asm_test = r"
+:loc_2
+    jmp loc_1
+
+
+:ret_2
+    divc
+    push 10
+    mulc
+    push 1
+    addc
+    push 26
+    subu
+    exit
+
+:main
+    push 2
+    push 5
+    addu
+    push 3
+    mulc
+    negu
+    push -4
+    jmp loc_2
+:loc_1
+    dbg_stack
+    swap
+    dbg_stack
+    jmp ret_2
+";
+
+        let tokens: Vec<Token> = Tokenizer::new(asm_test).tokenize().unwrap();
+        let parser = Parser::new(&tokens);
+        let (instructions, main_addr) = parser.parse().unwrap();
+        let mut vm = crate::vm::VM::new();
+        vm.load_program(instructions, main_addr);
+        let r = vm.run().unwrap();
+        assert_eq!(r, crate::vm::Value::Int(-25));
+    }
 }
